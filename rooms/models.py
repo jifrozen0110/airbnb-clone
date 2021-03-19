1459f86c93504daa7872c791b8927d2d53615c37
Jifrozen <answldjs1836@naver.com>
2021-03-20 02:52:55 +0900

    model 완료

--- rooms/models.py
+++ rooms/models.py
@@ -18,7 +18,41 @@ class AbstractItem(core_models.TimeStampModel):
 
 
 class RoomType(AbstractItem):
-    pass
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["created"]
+
+
+class Amenity(AbstractItem):
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(core_models.TimeStampModel):
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    """ HouseRule MOdel Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampModel):
+    """ Photo Model Deefinition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampModel):
@@ -39,7 +73,10 @@ class Room(core_models.TimeStampModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
         return self.name
