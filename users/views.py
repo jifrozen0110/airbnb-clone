16ef2eb2125f97d114eaf7cf4005340b1ce47fd6
Jifrozen <answldjs1836@naver.com>
2021-03-25 18:39:45 +0900

    signup Form

--- users/views.py
+++ users/views.py
@@ -19,6 +19,26 @@ class LoginView(FormView):
             login(self.request, user)
         return super().form_valid(form)
 
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
     # def get(self, request):
     #     form = forms.LoginForm()
     #     return render(request, {"form": form})
@@ -33,8 +53,3 @@ class LoginView(FormView):
     #             login(request, user)
     #             return redirect(reverse("core:home"))
     #     return render(request, "users/login.html", {"form": form})
-
-
-def log_out(request):
-    logout(request)
-    return redirect(reverse("core:home"))
